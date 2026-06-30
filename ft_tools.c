65e4a5075af44d7ad5797a6f045f63951179910a
natssj <166624923+natssj@users.noreply.github.com>
2026-06-30 15:17:26 +0200

    Update ft_tools.c
    Se ha añadido el include, hexauppercase y hexapointer

--- ft_tools.c
+++ ft_tools.c
@@ -9,6 +9,7 @@
 /*   Updated: 2026/06/30 14:33:48 by nde-dieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "ft_printf.h"
 
 char	ft_hexalowercase(unsigned int nbr)
 {
@@ -16,4 +17,26 @@ char	ft_hexalowercase(unsigned int nbr)
 
 	lower = "0123456789abcdef";
 	return (lower[nbr]);
+}
+
+char	ft_hexauppercase(unsigned int nbr)
+{
+	char	*upper;
+
+	upper = "0123456789ABCDEF";
+	return (upper[nbr]);
+}
+
+int	print_hexapointer(unsigned long nbr)
+{
+	char	word;
+	int		len;
+
+	len = 0;
+	if (nbr >= 16)
+		len = len + print_hexapointer(nbr / 16);
+	word = ft_hexalowercase(nbr % 16);
+	write(1, &word, 1);
+	len++;
+	return (len);
 }
